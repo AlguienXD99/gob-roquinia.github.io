21b7e5b28e1e1190c8edef6371d361340846a4a2
AlguienXD99 <m71284354@gmail.com>
2024-10-18 16:36:03 -0500

    Update cambio_contenido.js

--- cambio_contenido.js
+++ cambio_contenido.js
@@ -1,9 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
     const horaActual = new Date().getHours(); // Obtiene la hora actual
+    
+    const horaInicio = 16; // Cambia aquí la hora de inicio (ejemplo: 12 PM)
+    const horaFin = 17; // Cambia aquí la hora de fin (ejemplo: 1 PM)
+    
     const contenido = document.getElementById('contenido');
 
-    // Cambia la hora aquí (ejemplo: de 3 PM a 4 PM solo se muestra la imagen)
-    if (horaActual >= 15 && horaActual < 16) {
+    // Si la hora actual está dentro del rango definido, cambia el contenido a una imagen
+    if (horaActual >= horaInicio && horaActual < horaFin) {
         contenido.innerHTML = '<img src="imagen_secreta.jpg" alt="Imagen especial" style="display: block; margin: 0 auto; width: 100%; height: auto;">';
     }
 });
